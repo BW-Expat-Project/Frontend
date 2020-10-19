48cc073deb4446706e1d6cd932cff67d8373b965
KindredSoul <jabriltb2@gmail.com>
2020-10-19 18:41:43 -0500

    Added reducer

--- expat-journal/src/components/store/reducers/index.js
+++ expat-journal/src/components/store/reducers/index.js
@@ -1,4 +1,5 @@
 import axios from "axios"
+import {DATA_LOADING, FETCH_POSTS_SUCCESS, DELETE_POST, UPDATE_POST, DATA_FAIL } from "../actions"
 
 export const initialState = {
     posts: [],
@@ -11,7 +12,7 @@ export const initialState = {
     }
 }
 
-export const reducer = (state, action) => {
+export const reducer = (state=initialState, action) => {
     switch(action.type){
         default:
             return state
