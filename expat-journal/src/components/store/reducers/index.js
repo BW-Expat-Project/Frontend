7313c8c71bb5cf0f4f6a0ae01277861e1c8babf7
KindredSoul <jabriltb2@gmail.com>
2020-10-19 18:42:46 -0500

    Added reducer

--- expat-journal/src/components/store/reducers/index.js
+++ expat-journal/src/components/store/reducers/index.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import {DATA_LOADING, FETCH_POSTS_SUCCESS, DELETE_POST, UPDATE_POST, DATA_FAIL } from "../actions"
+import {DATA_LOADING, FETCH_DATA_SUCCESS, DELETE_POST, UPDATE_POST, DATA_FAIL } from "../actions"
 
 export const initialState = {
     posts: [],
@@ -14,6 +14,39 @@ export const initialState = {
 
 export const reducer = (state=initialState, action) => {
     switch(action.type){
+        case DATA_LOADING:
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            }
+        case FETCH_DATA_SUCCESS:
+            return {
+                ...state,
+                data: action.payload,
+                isLoading: false,
+                error: ''
+            }
+        case UPDATE_POST:
+            return {
+                ...state,
+                data: action.payload,
+                isLoading: false,
+                error: ''
+            }
+            
+        case DELETE_POST:
+            return {
+                ...state,
+                data: state.data.entries.filter(entry => entry.entry_id !== action.payload),
+                isLoading: false,
+                error: ''
+            }
+        case DATA_FAIL:
+            return {
+                ...state,
+                error: action.payload
+            }
         default:
             return state
     }
